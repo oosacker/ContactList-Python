941d4641b357a5ac856aa327f894b90a05e09d81
Natsuki <haseganats@myvuw.ac.nz>
2020-01-08 16:03:02 +1300

    test

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 
 Contacts = {}
-ContactNum = 0
 
 
 class Contact:
@@ -17,67 +16,66 @@ class Contact:
 
 
 def PrintContacts():
-    global Contacts
-    for Name in Contacts:
-        print(Contacts[Name])
+    if Contacts:
+        for Name in Contacts:
+            print(Contacts[Name])
+    else:
+        print('Contact list is empty\n')
 
 
 def AddContact():
-    sys.stdout.write('Name? ')
+    sys.stdout.write('Name?\n')
     name_str = sys.stdin.readline().strip()
 
-    sys.stdout.write('Phone? ')
+    sys.stdout.write('Phone?\n')
     phone_str = sys.stdin.readline().strip()
     try:
         int(phone_str)
     except ValueError:
-        print('Invalid phone number')
+        print('Invalid phone number\n')
         return
 
-    sys.stdout.write('Email? ')
+    sys.stdout.write('Email?\n')
     email_str = sys.stdin.readline().strip()
 
-    sys.stdout.write('occupation? ')
+    sys.stdout.write('occupation?\n')
     occ_str = sys.stdin.readline().strip()
 
-    Con = Contact(name_str, int(phone_str), email_str, occ_str)
-
-    global Contacts
-    Contacts[name_str] = Con
-
-    global ContactNum
-    ContactNum += 1
+    Contacts[name_str] = Contact(name_str, int(phone_str), email_str, occ_str)
 
 
 def FindContact():
-    print('Name of contact to search? ')
-    input_str = sys.stdin.readline().strip().casefold()
+    if Contacts:
+        print('Name of contact to search?\n')
+        input_str = sys.stdin.readline().strip().casefold()
 
-    global Contacts
-    if input_str in Contacts:
-        print(Contacts[input_str])
+        if input_str in Contacts:
+            print(Contacts[input_str])
+            return Contacts[input_str]
+        else:
+            print('Contact not found\n')
+
     else:
-        print('Contact not found')
-
+        print('Contact list is empty\n')
 
 def SaveContacts():
-    print('Name of new file? ')
+    print('Name of new file?\n')
     filename = sys.stdin.readline().strip().casefold()
     try:
-        global Contacts
         with open(filename, 'w') as fp:
             for Name in Contacts:
-                fp.write(Name + ',' + str(Contacts[Name].Phone) + ',' + Contacts[Name].Email + ',' + Contacts[Name].Occupation)
+                fp.write(Name + ',' + str(Contacts[Name].Phone) + ',' + Contacts[Name].Email + ',' + Contacts[Name].Occupation+'\n')
 
     except FileNotFoundError:
-        print("Loading file failed")
+        print("Loading file failed\n")
 
 
 def LoadContacts():
-    print('Name of file to load? ')
+    print('Name of file to load?\n')
     filename = sys.stdin.readline().strip().casefold()
     load_count = 0
     try:
+        global Contacts
         with open(filename) as fp:
             line = fp.readline()
             while line:
@@ -85,10 +83,26 @@ def LoadContacts():
                 Contacts[line[0]] = Contact(line[0], int(line[1]), line[2], line[3])
                 line = fp.readline()
                 load_count += 1
-        print('Number of contacts loaded: {}'.format(load_count))
+        print('Number of contacts loaded: {}\n'.format(load_count))
 
     except FileNotFoundError:
-        print("File not found")
+        print("File not found\n")
+
+
+def DeleteContact():
+    res = FindContact()
+
+    if not res:
+        return
+    else:
+        name = res.Name
+        del Contacts[name]
+        print(name + ' was deleted\n')
+
+
+def DeleteAllContacts():
+    Contacts.clear()
+    print('All contacts deleted\n')
 
 
 # Do not add () at the end of the function names!!!!
@@ -98,20 +112,28 @@ CommandList = {
     'find': FindContact,
     'save': SaveContacts,
     'load': LoadContacts,
+    'delete': DeleteContact,
+    'delete-all': DeleteAllContacts,
     'quit': quit
 }
 
 
 def UserInterface():
+    global CommandList
+
     while True:
-        sys.stdout.write('Command? ')
+        print('Your options:')
+        for cmd_name in CommandList:
+            print(cmd_name)
+
+        sys.stdout.write('\nCommand? ')
         command = sys.stdin.readline().strip().casefold()
 
-        if CommandList.__contains__(command):
+        if command in CommandList:
             CommandList[command]()
 
         else:
-            print('Invalid command')
+            print('Invalid command\n')
 
 
 UserInterface()
